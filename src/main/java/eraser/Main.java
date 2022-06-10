1a6200493cdf1421eca7cddf84344d8f25c4e21a
RepliKode <100425293+RepliKode@users.noreply.github.com>
2022-06-10 15:34:42 +0200

    Update Main.java

--- src/main/java/eraser/Main.java
+++ src/main/java/eraser/Main.java
@@ -3,9 +3,9 @@ package eraser;
 public class Main {
 
     public static void main(String[] args) {
-        String str = "T ex te  d e  Te st   : )";
-        str = str.replaceAll("(?<! ) (?! )", "");
-        System.out.println("résultats: " + str);
-        System.out.println(Eraser.erase(args[1]));
+        //On ajoute notre texte ici
+        String str = "Cou c ou   T ex te  d e   Te st   : )";
+        //affichage du résultat
+        System.out.println(Eraser.erase(str));
     }
 }
