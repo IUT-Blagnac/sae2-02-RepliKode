926fbd4a135d79921ff4a96edfae983173fac56f
RepliKode <100425293+RepliKode@users.noreply.github.com>
2022-06-10 15:22:21 +0200

    rajout du code
    manque les commentaires

--- src/main/java/eraser/Main.java
+++ src/main/java/eraser/Main.java
@@ -3,6 +3,9 @@ package eraser;
 public class Main {
 
     public static void main(String[] args) {
+        String str = "T ex te  d e  Te st   : )";
+        str = str.replaceAll("(?<! ) (?! )", "");
+        System.out.println("résultats: " + str);
         System.out.println(Eraser.erase(args[1]));
     }
 }
